12b719edd44dd17eb6d523acef6442731c161ba2
Ajayraj0911 <ajayrajnadar@gmail.com>
2021-02-02 22:50:37 +0530

    rd

--- src/com/controller/StudentController.java
+++ src/com/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +15,31 @@ public class StudentController extends HttpServlet {
 		
 		String studentName = request.getParameter("sName");
 		String studentEmail = request.getParameter("sEmail");
-		String studentPassword = request.getParameter("sPassword");
-		
-		System.out.println(studentName+"--"+studentEmail+"-"+studentPassword);
+		String age = request.getParameter("sAge");
+		int sAge = 0;
+		if (age != null && !age.equals("")) {
+
+			sAge = Integer.parseInt(age);
+		}
+		String gender = request.getParameter("gender");
+		String hobbies[] = request.getParameterValues("hobbies");
+		// int sAge = Integer.parseInt(request.getParameter("sAge"));
+		System.out.println(studentName + "--" + studentEmail + "-");
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("<h1>Student name =" + studentName + "</h1>");
+
+		out.print("<h2>Student email =" + studentEmail + "</h2>");
+		out.print("<h2>Student Age =" + sAge + "</h2>");
+		out.print("<font color = 'red' size ='4' >gender =" + gender + "</font>");
+		for (String s : hobbies) {
+
+			out.print("<h4>" + s + "</h4>");
+		}
+		out.print("</body></html>");
+		out.close();
 		
 		
 	}
