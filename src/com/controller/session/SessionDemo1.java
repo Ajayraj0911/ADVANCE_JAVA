fbf32633c91edfbad42498ec46c522d090b15121
Ajayraj0911 <ajayrajnadar@gmail.com>
2021-02-05 15:38:13 +0530

    session

---
+++ src/com/controller/session/SessionDemo1.java
@@ -0,0 +1,29 @@
+package com.controller.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionDemo1
+ */
+public class SessionDemo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//HTTPSESSION
+		HttpSession session = request.getSession();
+		//key value
+		String name="Ajayraj";
+		session.setAttribute("name",name);
+		System.out.println("Servlet 1"+session.getId());
+	}
+
+}
